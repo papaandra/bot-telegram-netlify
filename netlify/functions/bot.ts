d8cf0f9de9aaeb2248f453b5afd91b640109c938
papaandra <kacamatastore2010@gmail.com>
2025-08-19 20:38:39 +0700

    Update bot.ts

--- netlify/functions/bot.ts
+++ netlify/functions/bot.ts
@@ -1,13 +1,2 @@
-// deno-runtime
-// Mengimpor fungsi 'serve' dari library standar Deno.
-import { serve } from 'https://deno.land/std@0.224.0/http/server.ts';
-
-// Baris ini harus muncul di log Netlify Functions jika fungsi berjalan.
-console.log('Fungsi tes Deno dimulai dan siap merespons!');
-
-// Server Deno akan mendengarkan permintaan HTTP.
-serve(async (req) => {
-  console.log('Permintaan diterima. Metode:', req.method);
-  // Mengirim respons sukses.
-  return new Response('OK - Fungsi Tes Berjalan', { status: 200 });
-});
+[build]
+  functions = "netlify/functions"
